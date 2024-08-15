251246d637fda7865f7553e500a1f2d1de9b1ba9
linuskmr <linuskmr.dev@gmail.com>
2024-08-15 16:54:10 +0200

    Add handler for index, linking to the source code

--- main.go
+++ main.go
@@ -3,11 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	addr := ":8010"
+	http.HandleFunc("/", handleIndex)
 	http.HandleFunc("/github", Github)
 	log.Println("Listening on", addr)
 	http.ListenAndServe(addr, nil)
+}
+
+func handleIndex(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte(strings.ReplaceAll(`<html>
+	<head>
+		<meta charset="UTF-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body">
+		<pre style="text-align: center; margin: 3em 0;">
+		<b>webhooks.linu.sk</b><br>
+		Nothing to see here, except the <a href="https://github.com/linuskmr/webhooks.linu.sk">code</a>.
+		</pre>
+	</body>
+	</html>`, "\t", "")))
 }
